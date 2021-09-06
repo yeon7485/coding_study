16008b66df672d78e3373b5776c6f6aaef6c1386
yeon7485 <wsx4379@naver.com>
2021-09-06 18:51:58 +0900

    week 2

--- Week_02/SkillTree.java
+++ Week_02/SkillTree.java
@@ -1,12 +1,40 @@
+
 public class SkillTree {
     public int solution(String skill, String[] skill_trees){
         int answer = 0;
+        String[] st = new String[skill_trees.length];
+
+        // 배열 깊은 복사
+        for(int i = 0; i < skill_trees.length; i++){
+            st[i] = skill_trees[i];
+        }
+        
+        boolean tf = true;
         
         for(int i = 0; i < skill_trees.length; i++){
+            // skill에 들어가지 않는 스킬들 ""로 replace 해주기
+            for(int j = 0; j < skill_trees[i].length(); j++){
+                String s = Character.toString(skill_trees[i].charAt(j));
+                if(!skill.contains(s)){
+                    st[i] = st[i].replace(s, "");
+                }
+            }
             
-        }
+            // 스킬트리 순서 확인
+            for(int k = 0; k < st[i].length(); k++){
+                String s = Character.toString(st[i].charAt(k));
+                if(skill.indexOf(s) != k) {
+                    tf = false;
+                    break;
+                }
+            }
 
-        
+            if(tf == true) answer++;
+            else if(st[i].equals("")) answer++;
+
+            // 변수 초기화
+            tf = true;
+        }
         return answer;
     }
 
