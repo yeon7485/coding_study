803e3108c092aeefe2e10824920cea7fcdc0e8d2
yeon7485 <wsx4379@naver.com>
2021-09-08 18:32:05 +0900

    week 3

---
+++ Week_03/StringCompression.java
@@ -0,0 +1,15 @@
+public class StringCompression {
+    public int solution(String s) {
+        int answer = 0;
+        return answer;
+    }
+    public static void main(String[] args) {
+        StringCompression sc = new StringCompression();
+
+        System.out.println(sc.solution("aabbaccc"));
+        System.out.println(sc.solution("ababcdcdababcdcd"));
+        System.out.println(sc.solution("abcabcdede"));
+        System.out.println(sc.solution("abcabcabcabcdededededede"));
+        System.out.println(sc.solution("xababcdcdababcdcd"));
+    }    
+}
