a9f0d5e947ab7679507873151c14ebf7a7c2fbd6
yeon7485 <wsx4379@naver.com>
2021-11-12 20:26:16 +0900

    11.12

--- Level_2/PhoneNumList.java
+++ Level_2/PhoneNumList.java
@@ -3,28 +3,19 @@ import java.util.*;
 
 public class PhoneNumList {
     public boolean solution(String[] phone_book) {
-        boolean answer = true;
+        HashMap<String, Integer> map = new HashMap<>();
         for(int i = 0; i < phone_book.length; i++){
-            for(int j = i+1; j < phone_book.length; j++){
-                String temp = "";
-                if(phone_book[i].length() < phone_book[j].length()){
-                    temp = phone_book[j].substring(0, phone_book[i].length());
-                    if(phone_book[i].equals(temp)) {
-                        answer = false;
-                        return answer;
-                    }
-                }
-                else{
-                    temp = phone_book[i].substring(0, phone_book[j].length());
-                    if(phone_book[j].equals(temp)) {
-                        answer = false;
-                        return answer;
-                    }
+            map.put(phone_book[i], i);
+        }
+        for(int i = 0; i < phone_book.length; i++){
+            for(int j = 1; j < phone_book[i].length(); j++){
+                if(map.containsKey(phone_book[i].substring(0, j))) {
+                    return false;
                 }
             }
+            
         }
-
-        return answer;
+        return true;
     }
     public static void main(String[] args) {
         PhoneNumList pl = new PhoneNumList();
